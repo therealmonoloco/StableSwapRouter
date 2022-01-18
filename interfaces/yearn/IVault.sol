93ed0ca70acefdb17654022eae29af2c2bea7365
therealmonoloco <therealmonoloco@gmail.com>
2022-01-18 14:52:31 -0500

    feat: initial version

---
+++ interfaces/yearn/IVault.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: Copyright (c) Curve.Fi, 2021 - all rights reserved
+pragma solidity 0.6.12;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+
+interface IVault is IERC20 {
+    function token() external view returns (address);
+
+    function decimals() external view returns (uint256);
+
+    function deposit() external;
+
+    function pricePerShare() external view returns (uint256);
+
+    function withdraw(
+        uint256 amount,
+        address account,
+        uint256 maxLoss
+    ) external returns (uint256);
+}
